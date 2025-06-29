821e1a1739b8828b9815f9e80c55df6115115d63
Andrew <skkqw@yandex.ru>
2025-06-29 13:48:02 +0300

    Добавление моделей

--- app/migration/env.py
+++ app/migration/env.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 from app.core.config import database_url
 from app.dao.database import Base
+from app.auth.models import Role, User
 
 config = context.config
 config.set_main_option("sqlalchemy.url", database_url)
